40af87944a5bfea6b9b467565bc709b086d98c6b
Xianling(Lily) Zhang <alchemxz@gmail.com>
2018-02-04 23:17:29 -0800

    udpate if else decision making

---
+++ review_gg/10_decision_making.cpp
@@ -0,0 +1,20 @@
+//decison making
+//if else
+//if, else if, else
+
+#include<iostream>
+using namespace std:
+
+int main(){
+	int i=20;
+	if(i==10)
+		cout<<"i is 10\n";
+	else if(i==15)
+		cout<<"i is 15\n";
+	else if(i==20)
+		cout<<"i is 20\n";
+	else 
+		cout<<"is is not present";
+
+	return 0;
+}
