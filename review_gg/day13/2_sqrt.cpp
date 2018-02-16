a0f2293f9ad3f9534accf045342bea5bd5c8067b
Xianling(Lily) Zhang <alchemxz@gmail.com>
2018-02-15 16:02:00 -0800

    leetcode questions

---
+++ review_gg/day13/2_sqrt.cpp
@@ -0,0 +1,30 @@
+//2_sqrt.cpp
+/*
+题目：计算并返回平方根
+思路
+*/
+class Solution {
+public:
+    int mySqrt(int x) {
+        //some corner cases
+        if(x<1)
+            return 0;
+        else if(x<4)
+            return 1;
+        else{
+            int start=0, end=x/2+1;
+            while(start<=end){
+                int mid=(start+end)/2;  
+                if(x/mid==mid)
+                    return mid;
+                else if(mid<x/mid){
+                    start = mid+1;
+                }
+                else
+                    end = mid-1;
+            }
+            return end;
+        }  
+        
+    }
+};
---
+++ review_gg/day13/3_climbingStairs.cpp
@@ -0,0 +1,1 @@
+3_climbingStairs.cpp
