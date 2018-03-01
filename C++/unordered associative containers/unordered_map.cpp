74082a3c1337a1c0eb9410b679d6f4c41047039f
Xianling(Lily) Zhang <alchemxz@gmail.com>
2018-02-28 22:24:29 -0800

    update sequence container

--- C++/unordered associative containers/unordered_map.cpp
+++ C++/unordered associative containers/unordered_map.cpp
@@ -1,3 +1,21 @@
 #include<iostream>
 #include<string>
 #include<unordered_map>
+using namespace std;
+
+int main(){
+	unordered_map<string, int> mymap={
+		{"Mars",1},
+		{"Saturn",2},
+		{"Jupiter",2}
+	};
+	mymap.at("Mars") = 3397;
+	mymap.at("Saturn") +=10;
+	mymap.at("Jupiter") = mymap.at("Saturn") + 110;
+
+	for(auto& x: mymap){
+		cout<<x.first<<": "<<x.second<<endl
+	}
+
+	return 0;
+}
--- C++/unordered associative containers/unordered_set_swap.cpp
+++ C++/unordered associative containers/unordered_set_swap.cpp
@@ -5,5 +5,16 @@
 using namespace std;
 
 int main(){
-	
+	unordered_set<string> first ={"1","2","3"}, second={"wood","corn","oion"};
+	first.swap(second);
+
+	cout<<"first:\n";
+	for(const string& x: first) cout<<" "<<x;
+
+	cout<<"\n";
+
+	cout<<"second:\n";
+	for(const string& x: second) cout<<" "<<x;
+
+	return 0;
 }
